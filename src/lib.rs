d9c11dc5a029f7ac3c30e28a37d36166615cde1a
EinarJohansson <ejrobb09@gmail.com>
2021-01-28 10:47:20 +0100

    Multiplicera matriser

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use num::Float;
 use num::Num;
+use num::Zero;
 
 #[derive(Debug)]
 pub struct Matris<T: Num> {
@@ -35,11 +35,14 @@ impl<T: Num + Clone> Matris<T>{
 pub trait Operationer<T: Num> {
     fn addera(&mut self, matris: &Matris<T>) where T: std::ops::AddAssign + Copy;
     fn subtrahera(&mut self, matris: &Matris<T>) where T: std::ops::SubAssign + Copy;
-    fn multiplcera_skalar(&mut self, skalar: &T) where T: std::ops::MulAssign + Copy;
+    fn multiplicera_skalar(&mut self, skalar: &T) where T: std::ops::MulAssign + Copy;
+    fn multiplicera_matris(&mut self, matris: &Matris<T>)
+    where T: Zero + std::ops::AddAssign + std::ops::Mul + Copy;
 }
 
 impl<T: Num> Operationer<T> for Matris<T> {
-    fn addera(&mut self, matris: &Matris<T>) where T: std::ops::AddAssign + Copy, {
+    fn addera(&mut self, matris: &Matris<T>) 
+    where T: std::ops::AddAssign + Copy,{
         for (i, rad) in self.matris.iter_mut().enumerate() {
             for (j, kol) in rad.iter_mut().enumerate() {
                 *kol += matris.matris[i][j];
@@ -47,7 +50,8 @@ impl<T: Num> Operationer<T> for Matris<T> {
         }
     }
     
-    fn subtrahera(&mut self, matris: &Matris<T>) where T: std::ops::SubAssign + Copy, {
+    fn subtrahera(&mut self, matris: &Matris<T>)
+    where T: std::ops::SubAssign + Copy,{
         for (i, rad) in self.matris.iter_mut().enumerate() {
             for (j, kol) in rad.iter_mut().enumerate() {
                 *kol -= matris.matris[i][j];
@@ -55,11 +59,31 @@ impl<T: Num> Operationer<T> for Matris<T> {
         }
     }
     
-    fn multiplcera_skalar(&mut self, skalar: &T) where T: std::ops::MulAssign + Copy, {
+    fn multiplicera_skalar(&mut self, skalar: &T)
+    where T: std::ops::MulAssign + Copy,{
         for rad in self.matris.iter_mut() {
             for kol in rad.iter_mut() {
                 *kol *= *skalar;
             }
         }
+    }
+
+    fn multiplicera_matris(&mut self, matris: &Matris<T>)
+    where T: Zero + std::ops::AddAssign +std::ops::Mul + Copy {
+        assert_eq!(self.form.1, matris.form.0);
+
+        let (n, p) = (self.form.0, matris.form.1);
+        let mut c = vec![vec![T::zero(); p]; n];
+
+        for i in 0..n {
+            for j in 0..p {
+                let mut summa: T = T::zero();
+                for k in 0..self.form.1 {
+                    summa += self.matris[i][k] * matris.matris[k][j];
+                }
+                c[i][j] = summa;
+            }
+        }
+        *self = Matris::new(&c);
     }
 }
--- tests/test.rs
+++ tests/test.rs
@@ -4,31 +4,103 @@ mod tests {
     fn skapa_matriser() {
         let p: Vec<Vec<u8>> = vec![vec![2, 5, 3], vec![9, 8, 7]];
         let q: Vec<Vec<i32>> = vec![vec![-1, -5, 1], vec![4, 1, 0]]; 
-        let u: Vec<Vec<f32>> = vec![vec![1.1, 3.2]];
-        let w: Vec<Vec<f64>> = vec![vec![3.0, 4.2]];
+        let a: Vec<Vec<f32>> = vec![vec![1.1, 3.2]];
+        let b: Vec<Vec<f64>> = vec![vec![3.0, 4.2]];
     
-        let x = matrisimo::Matris::new(&p);
-        let y = matrisimo::Matris::new(&q);
-        let z = matrisimo::Matris::new(&u);
-        let q = matrisimo::Matris::new(&w);
+        let p = matrisimo::Matris::new(&p);
+        let q = matrisimo::Matris::new(&q);
+        let a = matrisimo::Matris::new(&a);
+        let b = matrisimo::Matris::new(&b);
         
-        assert_eq!(x.form, (2, 3));
-        assert_eq!(y.form, (2, 3));
-        assert_eq!(z.form, (1, 2));
-        assert_eq!(q.form, (1, 2));
-        
-        println!("{:?}", x);
-        println!("{:?}", y);
-        println!("{:?}", z);
-        println!("{:?}", q);
+        assert_eq!(p.form, (2, 3));
+        assert_eq!(q.form, (2, 3));
+        assert_eq!(a.form, (1, 2));
+        assert_eq!(b.form, (1, 2));
+    }
+    #[test]
+    fn addera_matriser() {
+        use matrisimo::Operationer;
 
-/*         y.multiplcera_skalar(&4);
-        
-        x.multiplcera_skalar(&3); // 4a
-        y.subtrahera(&x);
-    
-        println!("{:?}", y); 
+        let p: Vec<Vec<i32>> = vec![vec![2, 5, 3], vec![9, 8, 7]];
+        let q: Vec<Vec<i32>> = vec![vec![-1, -5, 1], vec![4, 1, 0]]; 
+        let pq_svar: Vec<Vec<i32>> = vec![vec![1, 0, 4], vec![13, 9, 7]];
 
-        assert_eq!(2 + 2, 4); */
+        let mut p = matrisimo::Matris::new(&p);
+        let q = matrisimo::Matris::new(&q);
+        
+        println!("P är lika med: {:?}", p);
+        println!("Q är lika med: {:?}", q);
+        p.addera(&q);
+        println!("P+Q är lika med: {:?}", p);
+
+        assert_eq!(p.matris, pq_svar);
+    }
+
+    #[test]
+    fn subtrahera_matriser() {
+        use matrisimo::Operationer;
+
+        let p: Vec<Vec<i32>> = vec![vec![2, 5, 3], vec![9, 8, 7]];
+        let q: Vec<Vec<i32>> = vec![vec![-1, -5, 1], vec![4, 1, 0]]; 
+        let pq_svar: Vec<Vec<i32>> = vec![vec![3, 10, 2], vec![5, 7, 7]];
+
+        let mut p = matrisimo::Matris::new(&p);
+        let q = matrisimo::Matris::new(&q);
+        
+        println!("P är lika med: {:?}", p);
+        println!("Q är lika med: {:?}", q);
+        p.subtrahera(&q);
+        println!("P-Q är lika med: {:?}", p);
+
+        assert_eq!(p.matris, pq_svar);
+    }
+
+    #[test]
+    fn multiplicera_matris_skalar() {
+        use matrisimo::Operationer;
+
+        let p: Vec<Vec<i32>> = vec![vec![2, 5, 3], vec![9, 8, 7]];
+        const Q:i32 = 34;
+        let pq_svar: Vec<Vec<i32>> = vec![vec![68, 170, 102], vec![306, 272, 238]];
+
+        let mut p = matrisimo::Matris::new(&p);
+
+        println!("P är lika med: {:?}", p);
+        println!("Q är lika med: {:?}", Q);
+        p.multiplicera_skalar(&Q);
+        println!("P*Q är lika med: {:?}", p);
+
+        assert_eq!(p.matris, pq_svar);
+    }
+
+    #[test]
+    fn multiplicera_matriser() {
+        use matrisimo::Operationer;
+        
+        let p: Vec<Vec<f64>> = vec![vec![2.0, 5.0], vec![2.0, 2.0]];
+        let q: Vec<Vec<f64>> = vec![vec![1.0, 2.0, 1.0], vec![1.0, 1.0, 0.0]]; 
+        let pq_svar: Vec<Vec<f64>> = vec![vec![7.0, 9.0, 2.0], vec![4.0, 6.0, 2.0]];
+
+        let a: Vec<Vec<i32>> = vec![vec![2, 5], vec![9, 8]];
+        let b: Vec<Vec<i32>> = vec![vec![-1, -5, 1], vec![4, 1, 0]]; 
+        let ab_svar: Vec<Vec<i32>> = vec![vec![18, -5, 2], vec![23, -37, 9]];
+        
+        let mut p = matrisimo::Matris::new(&p);
+        let q = matrisimo::Matris::new(&q);
+
+        let mut a= matrisimo::Matris::new(&a);
+        let b = matrisimo::Matris::new(&b);
+
+        println!("P är lika med: {:?}", p);
+        println!("A är lika med: {:?}", a);
+
+        p.multiplicera_matris(&q);
+        a.multiplicera_matris(&b);
+
+        println!("P är nu lika med: {:?}", p);
+        println!("A är nu lika med: {:?}", a);
+
+        assert_eq!(p.matris, pq_svar);
+        assert_eq!(a.matris, ab_svar);
     }
 }
